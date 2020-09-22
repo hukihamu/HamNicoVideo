dcfbe65584acaa70d891e1be4f7a7564301084de
hukihamu <kak19982626@yahoo.co.jp>
2020-09-22 23:46:24 +0900

    version: 0.1.3 fix: 保存できない問題の修正 add: Http動画のURLを自動でaタグに入れてくれる機能

--- js/nicovideo.js
+++ js/nicovideo.js
@@ -1,13 +1,49 @@
 const nicovideo = async function () {
 
-    const httpVideoButton = document.createElement('button')
-    httpVideoButton.innerText = 'HttpVideoURL'
+    function initInView(inView){
+        initHttpVideo()
+
+    }
+    function initHttpVideo(){
+        //http_video_url
+        const player = document.getElementById('MainVideoPlayer')
+        const setSrc = (src)=>{
+            const httpVideo = document.getElementById('http_video_url')
+            if (src.match('^https:') !== null){
+                httpVideo.href = src
+                httpVideo.removeAttribute('title')
+            }else {
+                httpVideo.removeAttribute('href')
+                httpVideo.title = '視聴方法がhttpではないため、利用できません。'
+            }
+        }
+        if (player !== undefined){
+            let video
+            if ((video = player.firstChild) !== null){
+                setSrc(video.src)
+            }
+            const callback = function (mutationsList) {
+                for (let mutation of mutationsList){
+                    setSrc(mutation.target.src)
+                }
+            }
+            new MutationObserver(callback).observe(player, {
+                subtree: true,
+                attributes: true,
+                attributeFilter: ['src']
+            })
+        }else {
+            console.error('MainVideoPlayerが見つかりません')
+        }
+    }
+
     const expOption =
         '<div class="Card">' +
         '   <div class="Card-header">' +
         '       <h1 class="Card-title">拡張オプション</h1>' +
         '   </div>' +
         '   <div class="Card-main">' +
+        '       <a id="http_video_url">HttpVideoURL</a>' +
         '   </div>' +
         '</div>'
 
@@ -17,6 +53,7 @@ const nicovideo = async function () {
 
     const sideGrid = document.getElementsByClassName('GridCell BottomSideContainer')[0]
     sideGrid.prepend(inView)
+    initInView(inView)
 }
 
 window.onload = nicovideo
